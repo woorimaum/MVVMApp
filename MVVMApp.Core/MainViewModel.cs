4e512b6478ebd7f5236851d13f417f03c3dc8bfa
ChoiYangHwan <yanghwan.choi@mobyus.com>
2024-12-04 11:07:43 +0900

    First Commit

---
+++ MVVMApp.Core/MainViewModel.cs
@@ -0,0 +1,26 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+
+namespace MVVMApp.Core
+{
+    public partial class MainViewModel : ObservableObject
+    {
+        //[ObservableProperty]
+        //public partial int Counter { get; set; }
+
+        [ObservableProperty]
+        public int counter;
+
+        [RelayCommand]
+        public void Increment()
+        {
+            Counter++;
+        }
+
+        [RelayCommand]
+        public void Decrement() 
+        {
+            Counter--;
+        }
+    }
+}
---
+++ MVVMApp.Test/Test1.cs
@@ -0,0 +1,32 @@
+﻿using MVVMApp.Core;
+
+namespace MVVMApp.Test
+{
+    [TestClass]
+    public sealed class Test1
+    {
+        [TestMethod]
+        public void TestIncrement()
+        {
+            MainViewModel mainViewModel = new();
+
+            Assert.AreEqual(0, mainViewModel.Counter);
+
+            mainViewModel.Increment();
+
+            Assert.AreEqual(1, mainViewModel.Counter);
+        }
+
+        [TestMethod]
+        public void TestDecrement() 
+        { 
+            MainViewModel mainViewModel = new();
+
+            Assert.AreEqual(0, mainViewModel.Counter);            
+
+            mainViewModel.Decrement();
+
+            Assert.AreEqual(-1, mainViewModel.Counter);
+        }
+    }
+}
